440bcc227f71978ba0e0018719befc1acfb3654b
ThomasSYLai <shaoyu.lai@utoledo.edu>
2021-04-20 11:39:29 -0400

    PAHFIT_v2.1

--- pahfit/base.py
+++ pahfit/base.py
@@ -1,6 +1,6 @@
 from astropy.modeling.functional_models import Gaussian1D
 
-from pahfit.component_models import BlackBody1D, S07_attenuation
+from pahfit.component_models import BlackBody1D, S07_attenuation, att_Drude1D
 
 from astropy.table import Table, vstack
 from astropy.modeling.physical_models import Drude1D
@@ -248,7 +248,59 @@ class PAHFITBase:
             fixed={"tau_sil": att_info["amps_fixed"][0]},
         )
 
+<<<<<<< HEAD
     def plot(self, axs, x, y, yerr, model, scalefac_resid=2):
+=======
+        # add additional att components to the model if necessary
+        # Note: perhaps can be improved to make it more concise by
+        #       encorporating them into a for loop
+        if 'tau_H2O' in att_info["names"]:
+            self.model *= att_Drude1D(
+                name=att_info["names"][1],
+                tau=att_info["amps"][1],
+                x_0=att_info["x_0"][1],
+                fwhm=att_info["fwhms"][1],
+                bounds={
+                    "tau": att_info["amps_limits"][1],
+                    "fwhm": att_info["fwhms_limits"][1],
+                },
+                fixed={
+                    "x_0": att_info["x_0_fixed"][1],
+                },
+            )
+
+        if 'tau_CO2' in att_info["names"]:
+            self.model *= att_Drude1D(
+                name=att_info["names"][2],
+                tau=att_info["amps"][2],
+                x_0=att_info["x_0"][2],
+                fwhm=att_info["fwhms"][2],
+                bounds={
+                    "tau": att_info["amps_limits"][2],
+                    "fwhm": att_info["fwhms_limits"][2],
+                },
+                fixed={
+                    "x_0": att_info["x_0_fixed"][2],
+                },
+            )
+
+        if 'tau_CO' in att_info["names"]:
+            self.model *= att_Drude1D(
+                name=att_info["names"][3],
+                tau=att_info["amps"][3],
+                x_0=att_info["x_0"][3],
+                fwhm=att_info["fwhms"][3],
+                bounds={
+                    "tau": att_info["amps_limits"][3],
+                    "fwhm": att_info["fwhms_limits"][3],
+                },
+                fixed={
+                    "x_0": att_info["x_0_fixed"][3],
+                },
+            )
+
+    def plot(self, ax, x, y, model):
+>>>>>>> 86cd523 (PAHFIT_v2.1)
         """
         Plot model using axis object.
 
@@ -267,6 +319,7 @@ class PAHFITBase:
         scalefac_resid : float
             Factor multiplying the standard deviation of the residuals to adjust plot limits
         """
+<<<<<<< HEAD
         # remove units if they are present
         if hasattr(x, "value"):
             x = x.value
@@ -303,6 +356,33 @@ class PAHFITBase:
                      mpl.lines.Line2D([0], [0], color="#DC267F", lw=2, alpha=0.5),
                      mpl.lines.Line2D([0], [0], color="#785EF0", lw=2, alpha=1),
                      mpl.lines.Line2D([0], [0], color="#FFB000", lw=2, alpha=0.5)]
+=======
+        ax.plot(x, model(x) / x, "g-")
+        ax.plot(x, y / x, "ks", fillstyle="none")
+
+        ax_att = ax.twinx()  # axis for plotting the extinction curve
+
+        # get the extinction model (probably a better way to do this)
+        for cmodel in model:
+            if isinstance(cmodel, S07_attenuation):
+                ext_components_S07 = cmodel
+
+        # get additional extinction components that can be
+        # characterized by functional forms (Drude profile in this case)
+        ext_components_funct = []
+        for cmodel in model:
+            if isinstance(cmodel, att_Drude1D):
+                ext_components_funct.append(cmodel)
+        ext_funct_model = ext_components_funct[0]
+        for cmodel in ext_components_funct[1:]:
+            ext_funct_model *= cmodel
+
+        ext_model = ext_components_S07(x) * ext_funct_model(x)
+
+        ax_att.plot(x, ext_model, "k--")
+        ax_att.set_ylabel("Attenuation")
+        ax_att.set_ylim(0, 1.1)
+>>>>>>> 86cd523 (PAHFIT_v2.1)
 
         # create the continum compound model (base for plotting lines)
         cont_components = []
@@ -453,6 +533,42 @@ class PAHFITBase:
             dtype=("U25", "U25", "float64", "float64", "float64", "bool"),
         )
 
+        # attenuation components that can be characterized by a functional form
+        att_funct_table = Table(
+            names=(
+                "Name",
+                "Form",
+                "x_0",
+                "x_0_min",
+                "x_0_max",
+                "x_0_fixed",
+                "amp",
+                "amp_min",
+                "amp_max",
+                "amp_fixed",
+                "fwhm",
+                "fwhm_min",
+                "fwhm_max",
+                "fwhm_fixed",
+            ),
+            dtype=(
+                "U25",
+                "U25",
+                "float64",
+                "float64",
+                "float64",
+                "bool",
+                "float64",
+                "float64",
+                "float64",
+                "bool",
+                "float64",
+                "float64",
+                "float64",
+                "bool",
+            ),
+        )
+
         for component in obs_fit:
             comp_type = component.__class__.__name__
 
@@ -563,11 +679,31 @@ class PAHFITBase:
                     ]
                 )
 
+            elif comp_type == "att_Drude1D":
+                att_funct_table.add_row(
+                    [
+                        component.name,
+                        comp_type,
+                        component.x_0.value,
+                        component.x_0.bounds[0],
+                        component.x_0.bounds[1],
+                        component.x_0.fixed,
+                        component.tau.value,
+                        component.tau.bounds[0],
+                        component.tau.bounds[1],
+                        component.tau.fixed,
+                        component.fwhm.value,
+                        component.fwhm.bounds[0],
+                        component.fwhm.bounds[1],
+                        component.fwhm.fixed,
+                    ]
+                )
+
         # Call featcombine to calculate combined dust feature strengths.
         cftable = featcombine(line_table)
 
         # stack the tables (handles missing columns between tables)
-        out_table = vstack([bb_table, line_table, att_table, cftable])
+        out_table = vstack([bb_table, line_table, att_table, att_funct_table, cftable])
 
         # Writing output table
         out_table.write(
@@ -603,7 +739,7 @@ class PAHFITBase:
         bb_ind = np.concatenate(np.argwhere(t["Form"] == "BlackBody1D"))
         df_ind = np.concatenate(np.argwhere(t["Form"] == "Drude1D"))
         ga_ind = np.concatenate(np.argwhere(t["Form"] == "Gaussian1D"))
-        at_ind = np.concatenate(np.argwhere(t["Form"] == "S07_attenuation"))
+        at_ind = np.concatenate(np.argwhere((t["Form"] == "S07_attenuation") | (t["Form"] == "att_Drude1D")))
 
         # now split the gas emission lines between H2 and ions
         names = [str(i) for i in np.take(t["Name"], ga_ind)]
@@ -690,11 +826,21 @@ class PAHFITBase:
         # Create the attenuation dict
         att_info = {
             "names": np.array(t["Name"][at_ind].data),
+            "x_0": np.array(t["x_0"][at_ind].data),
+            "x_0_limits": _ingest_limits(
+                t["x_0_min"][at_ind].data, t["x_0_max"][at_ind].data
+            ),
+            "x_0_fixed": _ingest_fixed(t["x_0_fixed"][at_ind].data),
             "amps": np.array(t["amp"][at_ind].data),
             "amps_limits": _ingest_limits(
                 t["amp_min"][at_ind].data, t["amp_max"][at_ind].data
             ),
             "amps_fixed": _ingest_fixed(t["amp_fixed"][at_ind].data),
+            "fwhms": np.array(t["fwhm"][at_ind].data),
+            "fwhms_limits": _ingest_limits(
+                t["fwhm_min"][at_ind].data, t["fwhm_max"][at_ind].data
+            ),
+            "fwhms_fixed": _ingest_fixed(t["fwhm_fixed"][at_ind].data),
         }
 
         # Create output tuple
