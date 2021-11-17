90fe94fbf05db10c306a17c1c8e9883faf4cbee9
Els Peeters <epeeters@uwo.ca>
2021-11-16 21:16:11 -0500

    -Added stellar continuum, combined atomic lines, combined atomic+H2 lines, and extinction model. -Removed trailing white space.

--- pahfit/helpers.py
+++ pahfit/helpers.py
@@ -8,7 +8,7 @@ from astropy.modeling.fitting import LevMarLSQFitter
 
 from pahfit.base import PAHFITBase
 
-from pahfit.component_models import BlackBody1D
+from pahfit.component_models import BlackBody1D, S07_attenuation
 from astropy.modeling.physical_models import Drude1D
 from astropy.modeling.functional_models import Gaussian1D
 
@@ -147,42 +147,48 @@ def fit_spectrum(obsdata, pmodel, maxiter=1000, verbose=True):
 
 def get_compounds(obsdata, pmodel):
     """
-    Determine model compounds for total continuum, dust continuum, dust feature, H2 lines
+    Determine model compounds for total continuum, stellar continuum, total dust continuum, combined dust features, 
+    combined atomic and H2 lines, combined H2 lines, combined atomic lines, and extinction model
 
     Parameters
     ----------
     obsdata : dict
         observed data where x = wavelength, y = SED, and unc = uncertainties
 
-    obs_fit : CompoundModel
-        PAHFIT model (output from fit_spectrum)
+    pmodel : PAHFITBase model
+        model giving all the components and parameters
 
     Returns
     -------
     compounds : dict
-        x = wavelength in microns; 
-        tot_cont = total continuum; 
-        dust_cont = total dust continuum; 
-        dust_feat = combined dust features; 
-        h2_feat = combined H2 lines
+        x = wavelength in microns;
+        tot_cont = total continuum;
+        stellar_cont = stellar continuum;
+        dust_cont = total dust continuum;
+        dust_features = combined dust features;
+        tot_lines = combined atomic and H2 emission lines;
+        h2_lines = combined H2 lines;
+        atomic_lines = combined atomic lines;
+        extinction_model = extinction model
     """
 
     # get wavelength array
     x = obsdata["x"].value
 
-    # calculate total dust continuum and total continuum (oncluding stellar continuum)
+    # calculate total dust continuum and total continuum (including stellar continuum)
+    # v2.0: first BlackBody1D is stellar continuum
     cont_components = []
 
     for cmodel in pmodel.model:
         if isinstance(cmodel, BlackBody1D):
             cont_components.append(cmodel)
     stellar_cont_model = cont_components[0]
-    dust_cont_model = cont_components[1] 
+    dust_cont_model = cont_components[1]
     for cmodel in cont_components[2:]:
         dust_cont_model += cmodel
     totcont = dust_cont_model(x) + stellar_cont_model(x)
 
-    # calculate total dust features 
+    # calculate total dust features
     dust_features = []
 
     for cmodel in pmodel.model:
@@ -193,22 +199,46 @@ def get_compounds(obsdata, pmodel):
         dust_features_model += cmodel
 
     # calculate H2 spectrum
-    H2_features = []
+    h2_features = []
 
     for cmodel in pmodel.model:
         if isinstance(cmodel, Gaussian1D):
             if cmodel.name[0:2] == 'H2':
-                H2_features.append(cmodel)
-    H2_features_model = H2_features[0]
-    for cmodel in H2_features[1:]:
-        H2_features_model += cmodel
+                h2_features.append(cmodel)
+    h2_features_model = h2_features[0]
+    for cmodel in h2_features[1:]:
+        h2_features_model += cmodel
+
+    # calculate atomic line spectrum
+    atomic_features = []
+
+    for cmodel in pmodel.model:
+        if isinstance(cmodel, Gaussian1D):
+            if cmodel.name[0:2] != 'H2':
+                atomic_features.append(cmodel)
+    atomic_features_model = atomic_features[0]
+    for cmodel in atomic_features[1:]:
+        atomic_features_model += cmodel
+
+    # all atomic and H2 lines
+    totlines = h2_features_model(x) + atomic_features_model(x)
+
+    # get extinction model
+    for cmodel in pmodel.model:
+        if isinstance(cmodel, S07_attenuation):
+            ext_model = cmodel(x)
 
     # save compounds in dictionary
     compounds = {}
     compounds["x"] = x
     compounds["tot_cont"] = totcont
+    compounds["stellar_cont"] = stellar_cont_model(x)
     compounds["dust_cont"] = dust_cont_model(x)
-    compounds["dust_feat"] = dust_features_model(x)
-    compounds["h2_feat"] = H2_features_model(x)
+    compounds["dust_features"] = dust_features_model(x)
+    compounds["tot_lines"] = totlines
+    compounds["h2_lines"] = h2_features_model(x)
+    compounds["atomic_lines"] = atomic_features_model(x)
+    compounds["extinction_model"] = ext_model
+
 
     return compounds
