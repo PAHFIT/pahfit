aefeca40d0ce426274d6f90d8bd424e9d3d93a9a
alexmaragko <maragkoudakis.alex@gmail.com>
2021-12-07 10:50:58 +0200

    combined PAH feature strengths calculation added

--- pahfit/feature_strengths.py
+++ pahfit/feature_strengths.py
@@ -1,5 +1,7 @@
 import numpy as np
+
 from astropy import constants as const
+from astropy.table import Table
 
 
 def pah_feature_strength(ampl, fwhm, x_0):
@@ -53,3 +55,53 @@ def line_strength(ampl, mean, stddev):
     strength = ((np.sqrt(np.pi / np.log(2))) * const.c.to('micron/s').value / 2) * (ampl * fwhm / mean**2) * 1e-26
 
     return strength
+
+def featcombine(ftable):
+        """
+        Combine dust features strengths.
+        Parameters
+        ----------
+        ftable : data table
+            Astropy table containing lines.
+        Returns
+        -------
+        cftable : data table
+            Astropy table containing combined dust feature strengths.
+        """
+        # Define combined strength dictionary.
+        cfdic = {'PAH_62': {'range': [6.2, 6.3]},
+                 'PAH_77_C': {'range': [7.3, 7.9]},
+                 'PAH_83': {'range': [8.3, 8.4]},
+                 'PAH_86': {'range': [8.6, 8.7]},
+                 'PAH_112_C': {'range': [11.2, 11.4]},
+                 'PAH_120': {'range': [11.9, 12.1]},
+                 'PAH_126_C': {'range': [12.6, 12.7]},
+                 'PAH_136': {'range': [13.4, 13.6]},
+                 'PAH_142': {'range': [14.1, 14.2]},
+                 'PAH_164': {'range': [16.4, 16.5]},
+                 'PAH_17_C': {'range': [16.4, 17.9]},
+                 'PAH_174': {'range': [17.35, 17.45]}
+                 }
+
+        # Create combined strength, unc, and eqw table.
+        cftable = Table(
+            names=("Name", "range_min", "range_max", "strength", "strength_unc", "eqw"),
+            dtype=("U25", "float64", "float64", "float64", "float64", "float64"))
+
+        # Get indices of dust features.
+        df_ind = np.concatenate(np.argwhere(ftable["Form"] == "Drude1D"))
+
+        # Get table subset containing only the dust features.
+        dfs = ftable[df_ind[0]:df_ind[-1] + 1]
+
+        # Iterate keys and calculate combined strengths.
+        for feat in cfdic.keys():
+            mask = np.logical_and(dfs['x_0'] >= cfdic[feat]['range'][0], dfs['x_0'] <= cfdic[feat]['range'][-1])
+            cftable.add_row([feat,
+                             cfdic[feat]['range'][0],
+                             cfdic[feat]['range'][-1],
+                             np.sum(dfs[mask]['strength']),
+                             None,
+                             None])
+
+        return cftable
