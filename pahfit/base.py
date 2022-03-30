89e06f84bb5321c1927e2b9ba0f64996b723f9d5
JD Smith <93749+jdtsmith@users.noreply.github.com>
2022-03-30 17:21:07 -0400

    Allow setting model_samples (1000 by default)

--- pahfit/base.py
+++ pahfit/base.py
@@ -270,7 +270,7 @@ class PAHFITBase:
                         },
                     )
 
-    def plot(self, axs, x, y, yerr, model, scalefac_resid=2):
+    def plot(self, axs, x, y, yerr, model, model_samples = 1000, scalefac_resid=2):
         """
         Plot model using axis object.
 
@@ -286,6 +286,8 @@ class PAHFITBase:
             observed spectrum uncertainties
         model : PAHFITBase model (astropy modeling CompoundModel)
             model giving all the components and parameters
+        model_samples : int
+            Total number of wavelength points to allocate to the model display
         scalefac_resid : float
             Factor multiplying the standard deviation of the residuals to adjust plot limits
         """
@@ -298,7 +300,7 @@ class PAHFITBase:
             yerr = yerr.value
 
         # Fine x samples for model fit
-        x_mod=np.linspace(min(x),max(x),1000)
+        x_mod=np.linspace(min(x),max(x),model_samples)
             
         # spectrum and best fit model
         ax = axs[0]
