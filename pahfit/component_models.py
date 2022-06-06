6bc6f85525d6725808036488c0d97a81d6590ac1
Thomas Lai <thomaslai78226@gmail.com>
2022-06-06 08:50:36 -0700

    fix S07 to tablulate form

--- pahfit/component_models.py
+++ pahfit/component_models.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pkg_resources
 from scipy import interpolate
 from astropy.modeling.physical_models import Drude1D
 from astropy.modeling import Fittable1DModel
 from astropy.modeling import Parameter
+from astropy.table import Table
 
 
 __all__ = ["BlackBody1D", "S07_attenuation", "att_Drude1D"]
@@ -58,44 +60,15 @@ class S07_attenuation(Fittable1DModel):
         """
         Output the kvt extinction curve
         """
-        # fmt: off
-        kvt_wav = np.array([8.0, 8.2, 8.4, 8.6, 8.8, 9.0, 9.2, 9.4, 9.6, 9.7,
-                            9.75, 9.8, 10.0, 10.2, 10.4, 10.6, 10.8, 11.0,
-                            11.2, 11.4, 11.6, 11.8, 12.0, 12.2, 12.4, 12.6,
-                            12.7])
 
-        kvt_int = np.array([.06, .09, .16, .275, .415, .575, .755, .895, .98,
-                            .99, 1.0, .99, .94, .83, .745, .655, .58, .525,
-                            .43, .35, .27, .20, .13, .09, .06, .045, .04314])
-        # fmt: on
+        kvt_prof = Table.read(pkg_resources.resource_filename("pahfit", "packs/")+'att_profile/kvt_Smith2007.ecsv')
 
-        # Extend kvt profile to shorter wavelengths
-        kvt_wav_short = in_x[in_x < min(kvt_wav)]
-        kvt_int_short_tmp = min(kvt_int) * np.exp(2.03 * (kvt_wav_short - min(kvt_wav)))
-        # Since kvt_int_shoft_tmp does not reach min(kvt_int),
-        # we scale it to stitch it.
-        kvt_int_short = kvt_int_short_tmp * (kvt_int[0] / max(kvt_int_short_tmp))
+        f = interpolate.interp1d(kvt_prof['lam'], kvt_prof['optical_depth'])
 
-        spline_x = np.concatenate([kvt_wav_short, kvt_wav])
-        spline_y = np.concatenate([kvt_int_short, kvt_int])
+        new_f = f(in_x)
 
-        # spline_rep = interpolate.splrep(spline_x, spline_y)
-        intfunc = interpolate.interp1d(spline_x, spline_y)
-        in_x_spline = in_x[in_x < max(kvt_wav)]
-        # new_spline_y = interpolate.splev(in_x_spline, spline_rep, der=0)
-        new_spline_y = intfunc(in_x_spline)
+        return new_f
 
-        nf = Drude1D(amplitude=0.4, x_0=18.0, fwhm=0.247 * 18.0)
-        in_x_drude = in_x[in_x >= max(kvt_wav)]
-
-        ext = np.concatenate([new_spline_y, nf(in_x_drude)])
-
-        # Extend to ~2 um
-        # assuing beta is 0.1
-        beta = 0.1
-        y = (1.0 - beta) * ext + beta * (9.7 / in_x) ** 1.7
-
-        return y
 
     def evaluate(self, in_x, tau_si):
         if tau_si == 0.0:
@@ -116,7 +89,8 @@ class att_Drude1D(Fittable1DModel):
     @staticmethod
     def evaluate(x, tau, x_0, fwhm):
         if tau == 0.0:
-            return np.full((len(x)), 0.0)
+            print(np.full((len(x)), 1.0))
+            return np.full((len(x)), 1.0)
         else:
             profile = Drude1D(amplitude=1.0, fwhm=fwhm, x_0=x_0)
             tau_x = tau * profile(x)
