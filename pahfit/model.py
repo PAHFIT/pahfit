f47599023ee8cf7e4b92dd01bf3a4bbbe5945db3
Dries Van De Putte <dvandeputte@stsci.edu>
2022-09-13 17:58:05 -0400

    param_info-based impl of new api (bar backporting the model result)

--- pahfit/model.py
+++ pahfit/model.py
@@ -3,6 +3,8 @@ from features import Features
 from base import PAHFITBase
 from astropy import units as u
 import copy
+from astropy.modeling.fitting import LevMarLSQFitter
+from matplotlib import pyplot as plt
 
 
 class Model:
@@ -114,12 +116,53 @@ class Model:
 
         """
         obs_x = spec.spectral_axis.to(u.micron).value
-        obs_y = spec.flux.value # TODO figure out right unit
+        obs_y = spec.flux.value  # TODO figure out right unit
 
         # remake param_info to make sure we have any feature updates from the user
-        param_info = PAHFITBase.parse_table(self.features)
+        param_info = self._kludge_param_info()
         param_info = PAHFITBase.estimate_init(obs_x, obs_y, param_info)
         self._backport_param_info(param_info)
+
+    def fit(self, spec: Spectrum1D, maxiter=1000, verbose=True):
+        """Create a model, fit it, and store the results in the features table
+
+        Parameters
+        ----------
+        spec : Spectrum1D
+
+        maxiter : int
+            maximum number of fitting iterations
+
+        verbose : boolean
+            set to provide screen output
+
+        """
+        astropy_model = self._construct_astropy_model()
+
+        # pick the fitter
+        fit = LevMarLSQFitter()
+
+        # fit
+        x = spec.spectral_axis.to(u.micon).value
+        y = spec.flux.value
+        w = 1.0 / spec.uncertainty.value
+        self.astropy_result = fit(
+            astropy_model,
+            x,
+            y,
+            weights=w,
+            maxiter=maxiter,
+            epsilon=1e-10,
+            acc=1e-10,
+        )
+        if verbose:
+            print(fit.fit_info["message"])
+
+        self._parse_astropy_result(self.astropy_result)
+
+    def info(self):
+        """Print out the last fit results."""
+        print(self.astropy_result)
 
     def plot(self, spec=None):
         """Plot model, and optionally compare to observational data.
@@ -130,31 +173,22 @@ class Model:
             Observational data. Does not have to be the same data that
             was used for guessing or fitting.
         """
-        pass
+        # copied some stuff from plot_pahfit
+        fig, axs = plt.subplots(
+            ncols=1,
+            nrows=2,
+            figsize=(15, 10),
+            gridspec_kw={"height_ratios": [3, 1]},
+            sharex=True,
+        )
 
-    def fit(self, spec: Spectrum1D):
-        """Create a model, fit it, and store the results in the features table"""
-        pass
+        x = spec.spectral_axis.to(u.micon).value
+        y = spec.flux.value
+        u = spec.uncertainty.value
+        astropy_model = self._construct_astropy_model()
+        PAHFITBase.plot(axs, x, y, u, astropy_model)
 
-    def _construct_astropy_model(self):
-        """Convert the features table into a fittable model."""
-        pass
-
-    def _parse_astropy_result(self, astropy_model):
-        """Store the result of the astropy fit into the features table
-
-        Every relevant value inside the astropy model, is written to the
-        right position in the features table. This way, the astropy
-        model and the features table are kept in sync.
-
-        Doing things this way, makes it possible for the user to make
-        edits to the features table, and makes it easy to store the
-        model (just store the features table)"""
-        pass
-
-    def info(self):
-        """Print out the last fit results."""
-        pass
+        fig.subplots_adjust(hspace=0)
 
     def copy(self):
         """Copy the model.
@@ -196,6 +230,17 @@ class Model:
         """
         self.features.write(fn)
 
+    def _kludge_param_info(self):
+        param_info = PAHFITBase.parse_table(self.features)
+        # edit line widths and drop lines out of range
+        param_info[2] = PAHFITBase.update_dictionary(
+            param_info[2], self.instrumentname, update_fwhms=True
+        )
+        param_info[3] = PAHFITBase.update_dictionary(
+            param_info[3], self.instrumentname, update_fwhms=True
+        )
+        return param_info
+
     def _backport_param_info(self, param_info):
         """Convert param_info to values in features table.
 
@@ -209,4 +254,27 @@ class Model:
         # But we can do something wacky here, like converting to model
         # first, and then back to table.
         astropy_model = PAHFITBase.model_from_param_info(param_info)
-        self._parse_astropy_result(model)
+        self._parse_astropy_result(astropy_model)
+
+    def _construct_astropy_model(self):
+        """Convert the features table into a fittable model."""
+        param_info = self._kludge_param_info()
+        return PAHFITBase.model_from_param_info(param_info)
+
+    def _parse_astropy_result(self, astropy_model):
+        """Store the result of the astropy fit into the features table
+
+        Every relevant value inside the astropy model, is written to the
+        right position in the features table. This way, the astropy
+        model and the features table are kept in sync.
+
+        Doing things this way, makes it possible for the user to make
+        edits to the features table, and makes it easy to store the
+        model (just store the features table)
+
+        """
+        # implementation inspired by pahfitbase. I think I better
+        # rewrite this thing from the start. Plenty of ways to test if
+        # it's correct.
+        raise NotImplementedError
+        pass
