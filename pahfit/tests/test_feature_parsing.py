4703751622617d508ab86a3b89f65c89c4d08515
Dries Van De Putte <dvandeputte@stsci.edu>
2022-09-20 17:36:40 -0400

    Update feature parsing test to new model class

--- pahfit/tests/test_feature_parsing.py
+++ pahfit/tests/test_feature_parsing.py
@@ -1,8 +1,7 @@
-import numpy as np
-from pahfit.base import PAHFITBase
+from pahfit.model import Model
 from pahfit.helpers import find_packfile
 from pahfit.features import Features
-from pahfit.instrument import wave_range
+
 
 def test_feature_parsing():
     """
@@ -22,16 +21,8 @@ def test_feature_parsing():
     features, ...) can deal with those feature not being there.
 
     """
-    # fake obsdata (will not actually do fitting in this test)
-    instrumentname = 'spitzer.irs.sl.2'
-    N = 100
-    wmin, wmax = wave_range(instrumentname)
-    obsdata = {
-        "x": np.linspace(wmin, wmax, N),
-        "y": np.linspace(1, 2, N),
-        "unc": np.full(N, 0.1),
-    }
-
+    # random instrument name
+    instrumentname = "spitzer.irs.sl.2"
 
     # choose any science pack
     packfile = find_packfile("classic.yaml")
@@ -39,27 +30,25 @@ def test_feature_parsing():
     # convert the yaml prescription to a features table
     features = Features.read(packfile)
 
-    # parse features table into the param_info dict, and init PAHFITBase
-    # object from it
-    def parse_and_init(features_instance):
-        param_info = PAHFITBase.parse_table(features_instance)
-        pmodel = PAHFITBase(
-            obsdata["x"],
-            obsdata["y"],
-            instrumentname,
-            estimate_start=True,
-            param_info=param_info,
-        )
-        return pmodel
+    def test_parsing(features_edit):
+        m = Model(features_edit, instrumentname, 0)
+        amodel = m._construct_astropy_model()
+        m._parse_astropy_result(amodel)
 
     # Case 0: the whole table
-    # whole_pmodel
-    _ = parse_and_init(features)
+    test_parsing(features)
 
-    # Case 1, 2, and 3 (no BB, Gauss, Drude)
-    remove_forms = ["BlackBody1D", "Gaussian1D", "Drude1D", "att_Drude1D"]
-    for kind in remove_forms:
-        _ = parse_and_init(features[features["kind"] != kind])
+    # Cases 1, 2, ... (can it run without any of the following?)
+    remove_kinds = [
+        "dust_continuum",
+        "dust_feature",
+        "line",
+        "starlight",
+        "attenuation",
+    ]
+    for kind in remove_kinds:
+        features_edit = features[features["kind"] != kind]
+        test_parsing(features_edit)
 
 
 if __name__ == "__main__":
