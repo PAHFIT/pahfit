f3393e1d42e94845b8434c6d291021cfb3fd22d3
Dries Van De Putte <dvandeputte@stsci.edu>
2022-11-07 18:19:17 -0500

    Force calculation of FWHM during guess, and guesser for dust feature

--- pahfit/model.py
+++ pahfit/model.py
@@ -5,10 +5,9 @@ from astropy.modeling.fitting import LevMarLSQFitter
 from matplotlib import pyplot as plt
 import numpy as np
 from scipy import interpolate, integrate
-import matplotlib as mpl
 
 from pahfit.features.util import bounded_is_fixed
-from pahfit.component_models import S07_attenuation, BlackBody1D
+from pahfit.component_models import BlackBody1D, Drude1D
 from pahfit.helpers import find_packfile
 from pahfit.features import Features
 from pahfit.base import PAHFITBase
@@ -137,7 +136,7 @@ class Model:
     def _repr_html_(self):
         return self._status_message() + self.features._repr_html_()
 
-    def guess(self, spec: Spectrum1D, redshift=None):
+    def guess(self, spec: Spectrum1D, redshift=None, calc_line_fwhm=True):
         """Make an initial guess of the physics, based on the given
         observational data.
 
@@ -180,10 +179,10 @@ class Model:
         sp = interpolate.interp1d(xz, yz)
 
         # we will repeat this loop logic several times
-        def loop_over_non_fixed(kind, parameter, estimate_function):
+        def loop_over_non_fixed(kind, parameter, estimate_function, force=False):
             for row_index in np.where(self.features["kind"] == kind)[0]:
                 row = self.features[row_index]
-                if not bounded_is_fixed(row[parameter]):
+                if not bounded_is_fixed(row[parameter]) or force:
                     guess_value = estimate_function(row)
                     # print(f"{row['name']}: setting {parameter} to {guess_value}")
                     self.features[row_index][parameter][0] = guess_value
@@ -206,6 +205,9 @@ class Model:
 
         loop_over_non_fixed("starlight", "tau", starlight_guess)
 
+        # count number of blackbodies in the model
+        nbb = len(self.features[self.features["kind"] == "dust_continuum"])
+
         def dust_continuum_guess(row):
             temp = row["temperature"][0]
             fmax_lam = 2898.0 / temp
@@ -220,26 +222,48 @@ class Model:
                 w = wmin
                 flux_ref = yz[np.argmin(xz)]
 
-            amp_guess = flux_ref / bb(w) * 0.2
-            return amp_guess
+            amp_guess = flux_ref / bb(w)
+            return amp_guess / nbb
 
         loop_over_non_fixed("dust_continuum", "tau", dust_continuum_guess)
 
-        def line_guess(row):
+        def line_fwhm_guess(row):
             w = row["wavelength"][0]
             if not instrument.within_segment(w, inst):
                 return 0
 
-            fwhm = instrument.fwhm(inst, w)[0][0]
-            xz_window = (w - 2 * fwhm < xz) & (xz < w + 2 * fwhm)
-            if np.count_nonzero(xz_window):
+            fwhm = instrument.fwhm(inst, w, as_bounded=True)[0][0]
+            return fwhm
+
+        def amp_guess(row, fwhm):
+            w = row["wavelength"][0]
+            if not instrument.within_segment(w, inst):
+                return 0
+
+            factor = 1.5
+            wmin = w - factor * fwhm
+            wmax = w + factor * fwhm
+            xz_window = np.logical_and(wmin < xz, xz < wmax)
+            xpoints = xz[xz_window]
+            ypoints = yz[xz_window]
+            if np.count_nonzero(xz_window) >= 2:
+                # difference between flux in window and flux around it
                 power_guess = integrate.trapezoid(yz[xz_window], xz[xz_window])
+                # subtract continuum estimate, but make sure we don't go negative
+                continuum = (ypoints[0] + ypoints[-1]) / 2 * (xpoints[-1] - xpoints[0])
+                if continuum < power_guess:
+                    power_guess -= continuum
             else:
                 power_guess = 0
-            amp_guess = power_guess / fwhm
-            return amp_guess
-
-        loop_over_non_fixed("line", "power", line_guess)
+            return power_guess / fwhm
+        
+        # calc line amplitude using instrumental fwhm and integral over data
+        loop_over_non_fixed(
+            "line", "power", lambda row: amp_guess(row, line_fwhm_guess(row))
+        )
+        # set the fwhms in the features table requested
+        if calc_line_fwhm:
+            loop_over_non_fixed("line", "fwhm", line_fwhm_guess, force=True)
 
     @staticmethod
     def _convert_spec_data(spec, z):
