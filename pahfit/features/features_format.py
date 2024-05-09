2729f89f1e2141f0d5d8314930824ce48cb8fa6c
JD Smith <93749+jdtsmith@users.noreply.github.com>
2024-05-09 13:30:01 -0400

    Make linter happier

--- pahfit/features/features_format.py
+++ pahfit/features/features_format.py
@@ -1,6 +1,7 @@
 import numpy as np
 from astropy.table.pprint import TableFormatter
 
+
 # * Special table formatting for bounded (val, min, max) values
 def fmt_func(fmt):
     def _fmt(x):
@@ -25,7 +26,7 @@ class BoundedParTableFormatter(TableFormatter):
         bpcols = []
         try:
             for col in table.columns.values():
-                if len(col.dtype) == 3: # bounded!
+                if len(col.dtype) == 3:  # bounded!
                     bpcols.append((col, col.info.format))
                     col.info.format = fmt_func(col.info.format or "g")
             return super()._pformat_table(table, *args, **kwargs)
